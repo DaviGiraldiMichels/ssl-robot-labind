55f38ba7b37f7e1c4d9c2d3b4624d75c6170236c
kohlbeck <264461450+kohlbeck@users.noreply.github.com>
2026-05-04 19:18:31 +0200

    Adicionando 'variabilidade' no main header.

--- include/main.h
+++ include/main.h
@@ -6,8 +6,22 @@
 extern "C" {
 #endif
 
+#if defined(STM32F1)
 #include <stm32f1xx_hal.h>
 #include <stm32f1xx_hal_conf.h>
+#elif defined(STM32F2)
+#include <stm32f2xx_hal.h>
+#include <stm32f2xx_hal_conf.h>
+#elif defined(STM32F3)
+#include <stm32f3xx_hal.h>
+#include <stm32f3xx_hal_conf.h>
+#elif defined(STM32F4)
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx_hal_conf.h>
+#else
+#error "No HAL drivers found!"
+#endif
+
 void Error_Handler(void);
 
 #ifdef __cplusplus
